f5e9bc15c63a7fb8fc86ff5a41d7a0ad36a4401d
blackhope58 <cnerzurum58xx@gmail.com>
2024-04-30 21:45:48 +0300

    design utils added

--- src/utils/colors.tsx
+++ src/utils/colors.tsx
@@ -1,9 +1,15 @@
 export type Theme = {
-  primary: string;
-  background: string;
+  black: string;
+  white: string;
+  red: string;
+  darkGray: string;
+  gray: string;
 };
 
 export const colors: Theme = {
-  primary: 'black',
-  background: '#FAFAFA',
+  black: '#000',
+  red: '#F40000',
+  white: '#fff',
+  darkGray: '#1D1E1C',
+  gray: '#979797',
 };
