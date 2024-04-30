0f0fa2e557858fd26de3ccddb18b2bc70df0126b
blackhope58 <cnerzurum58xx@gmail.com>
2024-04-30 21:46:44 +0300

    Bottom tabs navigations page changed

--- src/navigation/BottomTabsNavigation.tsx
+++ src/navigation/BottomTabsNavigation.tsx
@@ -1,9 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Home from '../screens/Home';
 import Detail from '../screens/Detail';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Text from '../components/Text';
+import {colors} from '../utils/colors';
 
 const {Navigator, Screen} = createMaterialTopTabNavigator();
 
@@ -15,6 +17,8 @@ const BottomTabsNavigation = () => {
       tabBarPosition="bottom"
       initialRouteName="Home"
       screenOptions={{
+        tabBarStyle: styles.container,
+        tabBarContentContainerStyle: styles.tabBarContentContainer,
         tabBarIndicator(props) {
           return null;
         },
@@ -26,8 +30,22 @@ const BottomTabsNavigation = () => {
           tabBarShowLabel: false,
           tabBarIcon: ({focused}) => {
             return (
-              <View style={{backgroundColor: 'black', width: 50, height: 50}}>
-                <Text>Home</Text>
+              <View style={styles.tabsContainer}>
+                <Image
+                  source={require('../assets/icons/discover.png')}
+                  style={[
+                    styles.icons,
+                    {tintColor: focused ? colors.black : colors.gray},
+                  ]}
+                />
+                <Text
+                  style={[
+                    styles.labelText,
+                    {color: focused ? colors.black : colors.gray},
+                  ]}
+                  fontWeight="bold">
+                  KEŞFET
+                </Text>
               </View>
             );
           },
@@ -40,8 +58,22 @@ const BottomTabsNavigation = () => {
           tabBarShowLabel: false,
           tabBarIcon: ({focused}) => {
             return (
-              <View style={{backgroundColor: 'black', width: 50, height: 50}}>
-                <Text>Detail</Text>
+              <View style={styles.tabsContainer}>
+                <Image
+                  source={require('../assets/icons/star.png')}
+                  style={[
+                    styles.icons,
+                    {tintColor: focused ? colors.black : colors.gray},
+                  ]}
+                />
+                <Text
+                  style={[
+                    styles.labelText,
+                    {color: focused ? colors.black : colors.gray},
+                  ]}
+                  fontWeight="bold">
+                  DAHA CÜZDAN
+                </Text>
               </View>
             );
           },
@@ -53,4 +85,40 @@ const BottomTabsNavigation = () => {
 
 export default BottomTabsNavigation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  icons: {
+    width: 28,
+    height: 28,
+    resizeMode: 'contain',
+    marginBottom: 8,
+  },
+  tabsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    flex: 1,
+    width: 100,
+  },
+  container: {
+    backgroundColor: colors.white,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
+  },
+  labelText: {
+    fontSize: 12,
+    textAlign: 'center',
+  },
+  tabBarContentContainer: {
+    height: 64,
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
+  },
+});
